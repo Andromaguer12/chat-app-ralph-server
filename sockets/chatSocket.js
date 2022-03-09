402727d74592c653ece55933e1d98214549f8c29
Andromaguer12 <andresj172020@gmail.com>
2022-03-09 18:39:53 -0400

    final commit (i think) bugfixes

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -4,8 +4,8 @@ module.exports = (io) => {
     io.on('connection', (socket) => {
         console.log("user connected")
         socket.on("messages", (data) => {
-            axios.post("http://localhost:8080/api/messages", data).then((res) => {
-                axios.get("http://localhost:8080/api/messages").then((response) => {
+            axios.post("https://chat-app-ralph.herokuapp.com/api/messages", data).then((res) => {
+                axios.get("https://chat-app-ralph.herokuapp.com/api/messages").then((response) => {
                     io.sockets.emit("new message", response.data.chat)
                 })
             })
